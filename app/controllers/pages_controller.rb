b52513d30008b0ec9559545606cc08e92f3a44be
alert2006 <mr_sva@mail.ru>
2011-07-08 08:27:30 +0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Homes"
   end
 
   def contact
+    @title = "Contacts"
+  end
+  
+  def about
+    @title = "About"
   end
 
 end
