aeb24a3b02902ee08efff55aeabcca2a135038a0
alert2006 <mr_sva@mail.ru>
2011-07-08 14:27:34 +0400

    Removed index.html in public folder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,23 @@
 class PagesController < ApplicationController
+  
   def home
+    @base_title = "My ROR site =)"
     @title = "Homes"
   end
 
   def contact
+    @base_title = "My ROR site =)"
     @title = "Contacts"
   end
   
   def about
+    @base_title = "My ROR site =)"
     @title = "About"
+  end
+  
+  def help
+    @base_title = "My ROR site =)"
+    @title = "Get helps"
   end
 
 end
